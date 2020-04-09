cc985aff40118d1f0dc79403cff41953ff60d0a2
jonesdebu <33497013+jonesdebu@users.noreply.github.com>
2020-04-09 11:09:22 -0500

    Basic Front-End UI
    Running on local host. Home page. /get_tweets  has a form where the user can enter a search term to get tweets related to the term. The user is then taken to another page /tweet_results to see the tweets related to the search term. Currently returning an exception every time

---
+++ flaskapp.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+import flask_restful
+from get_tweets import TwitterClient
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/get_tweets', methods = ['GET', 'POST'])
+def hastag_tweets():
+    if request.method =='GET':
+        return render_template('search_form.html')
+
+    elif request.method == 'POST':
+        api = TwitterClient()
+        form_text = request.form["text"]    # get form text
+        search_term = form_text.upper()  # process form text
+        data_set = api.buildTestSet(search_term)
+
+        return render_template(
+        'tweet_results.html',
+        response = data_set
+        )
+
+app.run()
