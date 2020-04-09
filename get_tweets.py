4a7567f2bcd8c4b92c48e11e2a7efb8de0aa64f5
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-04-08 22:28:54 -0400

    Added function to create test set (not working)

--- get_tweets.py
+++ get_tweets.py
@@ -1,12 +1,44 @@
+import re
 import tweepy
+from tweepy import OAuthHandler
 
-# Consumer keys and access tokens
-consumer_key = 'Ij5a4mBmBvNp4aiZ7F8uPhAWi'
-consumer_secret = 'DtrH2IGyq6KMds9zXzvsefd2knSee7K9xKqOdchvtTGuphCP2z'
-access_token = '931158103831216128-T4nbPvu1irazZ0t49MkZZqjT2xmQDSP'
-access_token_secret = 'y7WQZkM9jsiuQtmpopOc7rNctN910utOE3gDzyJaMYIgX'
 
-# Twitter API authorization and initilization
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+class TwitterClient(object):
+
+    def __init__(self):
+        # Consumer keys and access tokens
+        consumer_key = 'Ij5a4mBmBvNp4aiZ7F8uPhAWi'
+        consumer_secret = 'DtrH2IGyq6KMds9zXzvsefd2knSee7K9xKqOdchvtTGuphCP2z'
+        access_token = '931158103831216128-T4nbPvu1irazZ0t49MkZZqjT2xmQDSP'
+        access_token_secret = 'y7WQZkM9jsiuQtmpopOc7rNctN910utOE3gDzyJaMYIgX'
+
+    # Attempt authentication 
+        try: 
+            # Create OAuthHandler object 
+            self.auth = OAuthHandler(consumer_key, consumer_secret) 
+            # Set access token and secret 
+            self.auth.set_access_token(access_token, access_token_secret) 
+            # Create tweepy API object to fetch tweets 
+            self.api = tweepy.API(self.auth) 
+        except: 
+            print("Error: Authentication failed.")
+
+    def buildTestSet(self, search_keyword):
+        try:
+            tweets_fetched = self.api.GetSearch(search_keyword, count=100)
+            print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+            return [{"text":status.text, "label":None} for status in tweets_fetched]
+        except:
+            print("Unfortunately, something went wrong..")
+            return None
+
+def main():
+    api = TwitterClient()
+    search_term = input("Enter a search word:")
+    testDataSet = api.buildTestSet(search_term)
+    print(testDataSet[0:4])
+
+if __name__ == '__main__':
+    main()
+
+
